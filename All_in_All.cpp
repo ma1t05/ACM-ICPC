9e5057eb4e8ab0b0d74e38eba10e032d7982ecb9
ma1t05 <maltosla@gmail.com>
2016-01-12 14:37:10 -0600

    Ident acording emacs tabs

--- All_in_All.cpp
+++ All_in_All.cpp
@@ -1,30 +1,30 @@
-
+/* 10340 - All in All */
 #include <iostream>
 
 using namespace std;
 
 int main(){
-	string s,t;
-	int i, j;
-	bool s_in_t;
-	do{
-		cin >> s >> t;
-		for(i = 0,j = 0;s[i]!='\0';i++){
-			s_in_t = false;
-			for(;t[j]!='\0';j++){
-				if(t[j]==s[i]) {
-					s_in_t = true;
-					break;
-				}
-			}
-			if(s.size() - i > t.size() - j){
-				s_in_t = false;
-				break;
-			}
-			j++;
-		}
-		cout << (s_in_t ? "Yes" : "No");
-		if (cin.eof()) return 0;
-		cout << endl;
-	}while(1);
+  string s,t;
+  int i, j;
+  bool s_in_t;
+  cin >> s >> t;
+  do{
+    i = 0; j = 0;
+    s_in_t = false;
+    while(s[i] != '\0' && t[j] != '\0') {
+      while (t[j] != '\0') {
+	if (s[i] == t[j++]) {
+	  i++;
+	  break;
+	}
+      }
+      if (s[i] == '\0') {
+	s_in_t = true;
+	break;
+      }
+    }
+    cout << (s_in_t ? "Yes" : "No") << endl;
+    cin >> s >> t;
+  if (cin.eof()) return 0;
+  }while(1);
 }
